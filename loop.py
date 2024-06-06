3c6449d282ecc41ac4e56b7415b42d5257556f7d
luisfeliTEC-0603 <l.chaves.2@estudiantec.cr>
2024-06-06 16:12:31 -0600

    Update

--- loop.py
+++ loop.py
@@ -9,12 +9,25 @@ def raspberry(number):
 while True:
     try:
         
+        print("------------------------------------")
         user_input = int(input("Input .: "))
+        result = user_input - 3
 
         if 0 <= user_input < 8:
+                
+            print(f"Decimal -> {user_input} - 3 = {result}")
+
+            if result < 0:
+                result = 5 + user_input
+                print(f"[Número Negativo] Complemento a Dos de {result - 5} con tres dígitos -> {bin(user_input)[2:]} + 101 = {bin(result)[2:]}")
+                
+            print(f"Binario -> {bin(user_input)[2:]} - 011 = {bin(result)[2:]}")
+            
             raspberry(user_input)
+        
         else:
             print("-1 !Error : El número debe estar en el rango [0, 8[")
         
     except ValueError:
-        print("-1 !Error : Input inválido")
+        raspberry(None)
+        
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
 import machine
-import sys  
+import sys 
+import time
 
 # Hardware____________________________________________________________________________________________
 
 # LED's
 led = machine.Pin("LED", machine.Pin.OUT)
 
-provee = machine.Pin(0, machine.Pin.OUT)
+provee = machine.Pin(7, machine.Pin.OUT)
 
 pin_lst = [15, 16, 17]
 x_bit = [machine.Pin(pin, machine.Pin.OUT) for pin in pin_lst]
@@ -15,22 +16,32 @@ x_bit = [machine.Pin(pin, machine.Pin.OUT) for pin in pin_lst]
 # Main Loop____________________________________________________________________________________________
 
 loop = True
+
 while loop:
-
     user_input = sys.stdin.readline().strip()
-        
-    number = int(user_input)
-    binary = bin(number)[2:]
-    digs = list(map(int, binary))[::-1]
+    
+    if user_input == "None":
+        provee.value(0)
 
-    for index, led in enumerate(x_bit):
+    else:
+        provee.value(1)
 
         try:
-            led.value(digs[index])
-            print(f"x_bit[{index}] : {digs[index]} -> LED : {led}")
-        except IndexError:
-            led.value(0)
-            print(f"x_bit[{index}] : {0} -> LED : {led}")
+            number = int(user_input)
+            binary = bin(number)[2:]
+            digs = list(map(int, binary))[::-1]
+
+            for index, led in enumerate(x_bit):
+                try:
+                    led.value(digs[index])
+                    print(f"x_bit[{index}] : {digs[index]} -> LED : {led}")
+                except IndexError:
+                    led.value(0)
+                    print(f"x_bit[{index}] : {0} -> LED : {led}")
+        except ValueError:
+            pass
+    
+    time.sleep(0.1)  
 
 
 
